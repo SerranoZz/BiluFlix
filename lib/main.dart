a0bdee30e74946d487a06e333a7698bff4461c42
Lucas Serrano <lucas_serrano@id.uff.br>
2023-07-16 15:44:20 -0300

    começando tela de cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:catalogo_video/pages/login.dart';
+import 'package:catalogo_video/pages/signup.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Catálogo de Vídeos",
     debugShowCheckedModeBanner: false,
-    home: LoginPage(),
+    home: SignUp(),
   ));
 }
---
+++ lib/pages/signup.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment(0.00, -1.00),
+            end: Alignment(0, 1),
+            colors: [Colors.black, Color(0xE520083C)],
+          ),
+        ),
+        child: Center(
+          child: Text('Aa'),
+        ),
+      ),
+    );
+  }
+}
