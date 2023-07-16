86a730c86f35e46f4ca709ef30e5bf16f86ed236
Lucas Serrano <lucas_serrano@id.uff.br>
2023-07-16 14:39:40 -0300

    front tela de login

---
+++ lib/pages/login.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment(0.00, -1.00),
+            end: Alignment(0, 1),
+            colors: [Colors.black, Color(0xE520083C)],
+          ),
+        ),
+        child: Center(
+          child: Container(
+            height: 515,
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _buildText('Login', Colors.white, 30),
+                _buildText('Olá, seja bem-vindo(a)', Colors.white, 21),
+                _buildTextLabel('Email:', Colors.white, 300, false),
+                _buildTextLabel('Senha:', Colors.white, 300, true),
+                _buildButton('LOGIN', Color(0xFFFFF400), 300),
+                Container(
+                  width: 300,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _buildText('Ainda não tem cadastro?' , Colors.white, 16),
+                      GestureDetector(
+                        onTap: () {
+                          // Ação a ser executada quando o link for pressionado
+                        },
+                        child: Text(
+                          'Clique aqui',
+                          style: GoogleFonts.lexendDeca(
+                              textStyle: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xFFFFF400),
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ),        
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 300,
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        width: 1,
+                        strokeAlign: BorderSide.strokeAlignCenter,
+                        color: Color(0xFFF0F0F0),
+                      ),
+                    ),
+                  ),
+                ),
+                _buildButton('Entrar sem login', Colors.white, 300),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildText(String text, Color color, double size) {
+    return Text(
+      text,
+      style: 
+        GoogleFonts.lexendDeca(
+          textStyle: TextStyle(
+          fontSize: size,
+          fontWeight: FontWeight.w400,
+          color: color,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextLabel(String text, Color color, double width, senha){
+    return(
+      Container(
+        width: width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          Text(
+            text,
+            style: 
+              GoogleFonts.lexendDeca(
+                textStyle: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+
+          Container(
+            height: 40.0,
+            width: width,
+            child: TextField(
+              decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                contentPadding: EdgeInsets.only(left: 5),
+              ),
+              obscureText: senha,
+              style: GoogleFonts.lexendDeca(
+                textStyle: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w400,
+                color: Colors.black,
+              ),
+            ),
+            ),
+          ),
+        ]),
+      )
+    );
+  }
+
+  Widget _buildButton(String text, Color color, double width){
+    return ElevatedButton(
+        onPressed: () {
+          
+        },
+        child: 
+        Text(
+          text,
+          style: GoogleFonts.lexendDeca(
+                textStyle: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+        ),
+
+        style: ButtonStyle(
+          minimumSize: MaterialStateProperty.all(Size(width, 40.0)),
+          fixedSize: MaterialStateProperty.all(Size(width, 40.0)),
+          backgroundColor: MaterialStateProperty.all(color),
+        ),
+    );
+  }
+}
